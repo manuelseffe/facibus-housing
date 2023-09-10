abd2e90ebf34eeb23e86b110cee246c768475af3
dyCodes <yusufdaudu51@gmail.com>
2023-09-10 22:17:42 +0100

    feat: refactor view layout structure

--- public/js/app.js
+++ public/js/app.js
@@ -19325,6 +19325,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/scss/dashboard.scss":
+/*!***************************************!*\
+  !*** ./resources/scss/dashboard.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./node_modules/process/browser.js":
 /*!*****************************************!*\
   !*** ./node_modules/process/browser.js ***!
@@ -19641,6 +19654,7 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
+/******/ 			"css/dashboard": 0,
 /******/ 			"css/app": 0
 /******/ 		};
 /******/ 		
@@ -19691,8 +19705,9 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/scss/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/dashboard","css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/dashboard","css/app"], () => (__webpack_require__("./resources/scss/app.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/dashboard","css/app"], () => (__webpack_require__("./resources/scss/dashboard.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
---
+++ resources/views/dashboard/request-new.blade.php
@@ -0,0 +1,63 @@
+@extends('layout.dashboard')
+
+@section('content')
+  <div class="_heading">
+    <h1>Make a Request</h1>
+
+    <div class="back_btn">
+      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8 16L9.41 14.59L3.83 9L16 9L16 7L3.83 7L9.41 1.41L8 -6.99382e-07L6.99382e-07 8L8 16Z"
+          fill="#131313" />
+      </svg>
+    </div>
+  </div>
+
+  <form class="requestForm">
+    <div class="upload_group">
+      <div class="up_icon mb-3">
+        <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="21.5" cy="21.5" r="21.5" fill="#8C8C8C" />
+          <path
+            d="M30 14H14C12.895 14 12 14.895 12 16V28C12 29.105 12.895 30 14 30H30C31.105 30 32 29.105 32 28V16C32 14.895 31.105 14 30 14ZM15 27L18.5 22.5L21 25.51L24.5 21L29 27H15Z"
+            fill="white" />
+        </svg>
+      </div>
+
+      <p class="">Drag and drop a clear cover picture for this request</p>
+      <p class="h4 fw-bold my-4">OR</p>
+      <div class="btn btn-success">Browse</div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 col-md-6 form_group">
+        <label for="" class="form-label">Location</label>
+        <select class="form-select">
+          <option selected>-- Select --</option>
+          <option value="1">Lagos</option>
+        </select>
+      </div>
+
+      <div class="col-12 col-md-6 form_group">
+        <label for="" class="form-label">Target</label>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="0.00" aria-describedby="basic-addon2">
+          <span class="input-group-text" id="basic-addon2">₦</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="form_group">
+      <label for="" class="form-label">Request title</label>
+      <input type="email" class="form-control">
+    </div>
+
+    <div class="form_group">
+      <label for="" class="form-label">Description</label>
+      <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+
+    <div class="form_group">
+      <button class="btn btn-success">Submit</button>
+    </div>
+  </form>
+@endsection
